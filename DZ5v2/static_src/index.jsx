9f3291687ab32debf0639e41666f00b7142d4fc1
LightAlykard <lightalykard@yandex.ru>
2021-02-19 00:16:57 +0300

    HW-5
    Все сделано, только плагин для проверки не встает

---
+++ DZ5v2/static_src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react' ;
+import ReactDOM from 'react-dom' ;
+import App from './components/MessageField';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './components/Router';
+import { Provider } from 'react-redux' ;
+import initStore from './utils/store' ;
+
+ReactDOM.render (
+    <Provider store = { initStore () } >
+        <BrowserRouter>
+            <MuiThemeProvider>
+                <Router />
+            </MuiThemeProvider>
+        </BrowserRouter>
+    </Provider>, 
+    document. getElementById ( 'root' ) ,
+);
